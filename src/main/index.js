49c5539945c1b7cd1953b391ec51d3a777d1cab1
彥彬 <sl8520@hotmail.com>
2020-02-11 16:16:57 +0800

    update electron

--- src/main/index.js
+++ src/main/index.js
@@ -21,6 +21,9 @@ function createWindow() {
     height: 563,
     useContentSize: true,
     width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   })
 
   mainWindow.loadURL(winURL)
