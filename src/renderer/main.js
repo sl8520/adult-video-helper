cff10ecdc5127c7922b2476e542cbaea801e6793
彥彬 <sl8520@hotmail.com>
2020-02-18 17:45:27 +0800

    element ui 使用繁體語系

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/zh-TW'
 import './styles/element-variables.scss'
 
 import '@/styles/index.scss' // global css
@@ -9,7 +10,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-Vue.use(Element)
+Vue.use(Element, { locale })
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
